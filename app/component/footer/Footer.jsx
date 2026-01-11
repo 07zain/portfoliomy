09311a1508e856b84d2ffb1fdd4edbd03edf2280
Zainab Ashraf <106245169+07zain@users.noreply.github.com>
2026-01-11 20:41:04 +0530

    Fix LinkedIn link formatting in Footer component

--- app/component/footer/Footer.jsx
+++ app/component/footer/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="https://www.linkedin.com/in/ashrafzainab
+            href="https://www.linkedin.com/in/ashrafzainab"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="LinkedIn"
